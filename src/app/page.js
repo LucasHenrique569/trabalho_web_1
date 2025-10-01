e20c62bacfdfd3d7efa03181893c5d04baacdde7
LucasHenrique569 <cs206838@gmail.com>
2025-09-30 20:01:49 -0400

    atualizando_a_pagina_inicial

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,53 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
+import { useState } from "react";
+
+export default function Page() {
+  const [nome, setNome] = useState("");
+  const [matricula, setMatricula] = useState("");
+  const [curso, setCurso] = useState("");
+
+  const salvarDados = async (event) => {
+    try {
+      event.preventDefault();
+      const resposta = await fetch("/api/alunos", {
+        method: "POST",
+        headers: { "Content-type": "Application/json" },
+        body: JSON.stringify({ nome: nome, matricula: matricula, curso: curso })
+      });
+
+      if(resposta.ok){
+        alert("Deu certo, show na bagaça")
+      }
+
+    } catch (err) {
+      return console.error("Deu erro na bagaça ", err);
+    }
+  }
+
   return (
-    <div>
-      <h1>Bem vindo ao site de livros lidos</h1>
-    </div>
+    <main>
+      <form onSubmit={salvarDados}>
+        <input 
+          type="text"
+          placeholder="Digite o nome" 
+          onChange={(event) => setNome(event.target.value)} 
+        />
+
+        <input
+          type="number"
+          placeholder="Digite a matrícula"
+          onChange={(event) => setMatricula(event.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Digite o curso"
+          onChange={(event) => setCurso(event.target.value)}
+        />
+        
+        <button>Salvar</button>
+      </form>
+    </main>
   );
 }
